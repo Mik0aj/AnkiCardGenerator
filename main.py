8af748b2c73364994d14d392b8ada4e8776aa97b
Mik0aj <mikoajmoson@gmail.com>
2021-02-10 13:06:36 +0100

    Extracted methods from main function so that the code is more redable

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from google_speech import Speech
 import os
 import requests
 import re
-src="cs"
-dest ="en"
-wordsArray = []
-
+SAMPLE_TXT_NAME= 'sample.txt'
 RECORD_FOLDER ='recording'
 IMAGE_FOLDER='images'
+# I don't know how to get images from google images, but getting images from Unsplash site was quiet easy
 GOOGLE_IMAGE = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
 STOCK_IMAGE='https://unsplash.com/s/photos/'
 usr_agent = {
@@ -19,62 +17,73 @@ usr_agent = {
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive',
 }
-def download_records():
+
+src="cs"
+dest ="en"
+
+def download_records(wordsArray):
     if not os.path.exists(RECORD_FOLDER):
         os.mkdir(RECORD_FOLDER)
-    for word in wordsArray:
+    for word in wordsArray[0]:
         speach=Speech(word,src)
+        print("Recoring for {start} : downloading mp3 and saving it in {folder}/{name}.jpg".format(
+            start=word,
+             folder=RECORD_FOLDER, name=word))
+
         speach.save("{}/{}.mp3".format(RECORD_FOLDER,word))
 
 
-# https:\/\/images\.unsplash\.com\/photo-[^;]*
-def download_images(links):
+def download_images(links,wordsArray):
     for i,link in enumerate(links):
+        word=wordsArray[0][i]
         response = requests.get(link, headers=usr_agent)
         html = response.text
         regex = r"https:\/\/images\.unsplash\.com\/photo-[^;]*"
         matches = re.search(regex, html, re.MULTILINE)
         if matches:
-            print("Match was found at {start}-{end}: {match}".format(start=matches.start(), end=matches.end(),
-                                                                     match=matches.group()))
+            print("Match was found for {start} : {match} downloading image and saving it in {folder}/{name}.jpg".format(start=word,
+                                                                     match=matches.group(),folder=IMAGE_FOLDER,name=word))
             response = requests.get(matches.group())
-
-            file = open("{}/{}.jpg".format(IMAGE_FOLDER,newList[0][i]), "wb")
+            file = open("{}/{}.jpg".format(IMAGE_FOLDER,word), "wb")
             file.write(response.content)
             file.close()
-            for groupNum in range(0, len(matches.groups())):
-                groupNum = groupNum + 1
-
-                print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum=groupNum,
-                                                                                start=matches.start(groupNum),
-                                                                                end=matches.end(groupNum),
-                                                                                group=matches.group(groupNum)))
 
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    translator = Translator()
-    print(translator.translate("word"))
-    file1 = open('sample.txt', 'r')
-    Lines = file1.readlines()
-    count =0
+def extract_words(name):
+    file = open(name, 'r')
+    Lines = file.readlines()
+    count = 0
+    words=[]
     for line in Lines:
         count += 1
-        wordsArray.append(line.strip())
-        #print("Line{}: {}".format(count, line.strip()))
-    download_records()
-    translations = translator.translate(wordsArray, src=src,dest=dest)
-    newList=[[],[]]
-    links=[]
+        words.append(line.strip())
+    file.close()
+    return words
+
+
+def translate_words(wordsArray):
+    translator = Translator()
+    translations = translator.translate(wordsArray[0], src=src, dest=dest)
+    translated_words=[]
     for translation in translations:
-        newList[0].append(translation.origin)
-        newList[1].append(translation.text)
-    print(newList)
-    for word in newList[1]:
-        #space needs to be replaced by + otherwise google searches only part of the phrase
+        translated_words.append(translation.text)
+    return translated_words
+
+
+def generate_image_links(wordsArray):
+    links = []
+    for word in wordsArray[1]:
+        # space needs to be replaced by "+" otherwise google searches only part of the phrase in case of Unsplash replace with "-"
         searchurl = STOCK_IMAGE + word.replace(" ", "-")
         links.append(searchurl)
-    download_images(links)
+    return links
+
+
+if __name__ == '__main__':
+    wordsArray = [[], []]
+    wordsArray[0]=extract_words(SAMPLE_TXT_NAME)
+    wordsArray [1]=translate_words(wordsArray)
+    download_records(wordsArray)
+    download_images(generate_image_links(wordsArray),wordsArray)
 
 
