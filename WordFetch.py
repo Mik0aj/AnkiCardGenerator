463439e34919439a668ea2b86be2193f119da44d
Mik0aj <mikoajmoson@gmail.com>
2021-02-10 17:14:42 +0100

    Main.py now generates simple anki deck, unforunetly without sound nor images Besides that i have reformated the code and added some comments

--- WordFetch.py
+++ WordFetch.py
@@ -5,23 +5,26 @@ import requests
 from google_speech import Speech
 from googletrans import Translator
 
+
 # I don't know how to get images from google images, but getting images from Unsplash site was quiet easy
-#GOOGLE_IMAGE = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
-#STOCK_IMAGE='https://unsplash.com/s/photos/' the address is hardcoded into a class
+# GOOGLE_IMAGE = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
+# STOCK_IMAGE='https://unsplash.com/s/photos/' the address is hardcoded into a class
 
 class Word_Fetcher():
-    def __init__(self,record_folder,image_folder,file_name,usr_agent,src="auto",dest="en",):
+    def __init__(self, record_folder, image_folder, file_name, usr_agent, src="auto", dest="en", ):
         # Source language, Destination language, name of record, name of image
-        self.wordsArray = [[], [],[],[]]
-        self.file_name=file_name
-        self.src=src
-        self.dest=dest
+        self.wordsArray = [[], [], [], []]
+        self.file_name = file_name
+        self.src = src
+        self.dest = dest
         self.__extract_words()
         self.__translate_words()
         self.__download_records(record_folder)
-        self.__download_images(self.__generate_image_links(), image_folder,usr_agent)
-        self.wordsArray[2]=["{}/{}.mp3".format(record_folder,x) for x in self.wordsArray[0]]
-        self.wordsArray[3]=["{}/{}.jpg".format(image_folder,x) for x in self.wordsArray[0]]
+        self.__download_images(self.__generate_image_links(), image_folder, usr_agent)
+        # All images and recordings are stored in designeted folders. They don't change during runtime.
+        # So to save up memory i have decided to use generetors to store the name and address of all jpg and mp3.
+        self.wordsArray[2] = ["{}/{}.mp3".format(record_folder, x) for x in self.wordsArray[0]]
+        self.wordsArray[3] = ["{}/{}.jpg".format(image_folder, x) for x in self.wordsArray[0]]
 
     def __extract_words(self):
         file = open(self.file_name, 'r')
@@ -40,25 +43,26 @@ class Word_Fetcher():
         if not os.path.exists(record_folder):
             os.mkdir(record_folder)
         for word in self.wordsArray[0]:
-            speach=Speech(word,self.src)
+            speach = Speech(word, self.src)
             print("Recoring for {start} : downloading mp3 and saving it in {folder}/{name}.jpg".format(
                 start=word,
-                 folder=record_folder, name=word))
-            speach.save("{}/{}.mp3".format(record_folder,word))
-
+                folder=record_folder, name=word))
+            speach.save("{}/{}.mp3".format(record_folder, word))
 
     def __download_images(self, links, image_folder, usr_agent):
-        for i,link in enumerate(links):
-            word=self.wordsArray[0][i]
+        for i, link in enumerate(links):
+            word = self.wordsArray[0][i]
             response = requests.get(link, headers=usr_agent)
             html = response.text
             regex = r"https:\/\/images\.unsplash\.com\/photo-[^;]*"
             matches = re.search(regex, html, re.MULTILINE)
             if matches:
-                print("Match was found for {start} : {match} downloading image and saving it in {folder}/{name}.jpg".format(start=word,
-                                                                         match=matches.group(),folder=image_folder,name=word))
+                print(
+                    "Image was found for {start} : {match} downloading image and saving it in {folder}/{name}.jpg".format(
+                        start=word,
+                        match=matches.group(), folder=image_folder, name=word))
                 response = requests.get(matches.group())
-                file = open("{}/{}.jpg".format(image_folder,word), "wb")
+                file = open("{}/{}.jpg".format(image_folder, word), "wb")
                 file.write(response.content)
                 file.close()
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import genanki
+
 from WordFetch import Word_Fetcher
 
-SAMPLE_TXT_NAME= 'sample.txt'
-RECORD_FOLDER ='recording'
-IMAGE_FOLDER='images'
-SRC="cs"
-DEST ="en"
+SAMPLE_TXT_NAME = 'sample.txt'
+RECORD_FOLDER = 'recording'
+IMAGE_FOLDER = 'images'
+SRC = "cs"
+DEST = "en"
 USER_AGENT = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -13,8 +15,33 @@ USER_AGENT = {
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive',
 }
+my_model = genanki.Model(
+    1380120064,
+    'Card generator model',
+    fields=[
+        {'name': 'Question'},
+        {'name': 'Answer'},
 
+    ],
+    templates=[
+        {
+            'name': 'Card 1',
+            'qfmt': '{{Question}}<br>',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+        },
+    ])
+my_deck = genanki.Deck(
+    2059400110,
+    'Czech lang')
 
 if __name__ == '__main__':
-    wf= Word_Fetcher(record_folder=RECORD_FOLDER, image_folder=IMAGE_FOLDER, file_name=SAMPLE_TXT_NAME, src=SRC, dest=DEST, usr_agent=USER_AGENT)
-    print(wf.get_data())
+    wf = Word_Fetcher(record_folder=RECORD_FOLDER, image_folder=IMAGE_FOLDER, file_name=SAMPLE_TXT_NAME, src=SRC,
+                      dest=DEST, usr_agent=USER_AGENT)
+    anki_data = wf.get_data()
+    for i in range(len(anki_data[0])):
+        my_note = genanki.Note(
+            model=my_model,
+            fields=[anki_data[0][i], anki_data[1][i]])
+        my_deck.add_note(my_note)
+    my_package = genanki.Package(my_deck)
+    my_package.write_to_file('output.apkg')
